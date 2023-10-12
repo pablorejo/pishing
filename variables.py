b9f45afbe257d41fd261361333a008ef72324238
PRI <p.rejo@sixtema.es>
2023-10-12 20:22:30 +0200

    Añadiendo readme

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
 # Correo desde el que vamos a enviar los mensajes
-CORREO_EMISOR = "pablopiorejoiglesias@gmail.com"
+CORREO_EMISOR = "tu-correo@lo-que-sea"
 
 # Configuracion del servidor SMTP
-EMAIL_SMPT = "smtp.gmail.com"
+EMAIL_SMPT = "smtp.gmail.com" 
 PUERTO_SMPT = '587'
 
 # Modelo que vamos a usar en el chat
@@ -12,12 +12,11 @@ MODELO_DEL_CHAT = Modelos_de_chat[2]
 # Elegir si queremos un objetivo en concreto o queremos que se generen automaticamente
 GENERAR_OBJETIVOS = True
 
-if (not GENERAR_OBJETIVOS): 
-    # En caso de que queramos que sea con un objetivo en concreto
-    NOMBRE_PERSONA_EMPRESA = "Geremias Atrusti"
-    NOMBRE_EMPRESA = "Perros sin casa"
-    IMAGEN_EMPRESA = "https://imgmedia.larepublica.pe/640x371/larepublica/original/2022/07/27/62e168ce1c95403984781dba.webp"
-    OBJETIVO_EMPRESA = "Eres una organizacion que busca ayudar a los perros callejeros"
+# Numero de empresas a generar
+NUMERO_DE_EMPRESAS = 3
+
+#Nombre de la persona que enviara el correo
+NOMBRE_PERSONA_EMPRESA = "Geremias Atrusti"
 
 #Url a la que se enlazara con la donacion
 URL_DONACION = "ayudanos.com"
@@ -26,13 +25,20 @@ URL_DONACION = "ayudanos.com"
 NUMERO_DE_LINEAS_MIN = 100
 NUMERO_DE_LINEAS_MAX = 200
 
+
+if (not GENERAR_OBJETIVOS): 
+    # En caso de que queramos que sea con un objetivo en concreto
+    NOMBRE_EMPRESA = "Perros sin casa"
+    IMAGEN_EMPRESA = "https://imgmedia.larepublica.pe/640x371/larepublica/original/2022/07/27/62e168ce1c95403984781dba.webp"
+    OBJETIVO_EMPRESA = "Eres una organizacion que busca ayudar a los perros callejeros"
+
+
+
 # Numero de imagenes maximo en caso de generar automaticamente las empresas
 NUMERO_DE_IMAGENES = 1
 # Carpeta donde se guardaran las imagenes
 CARPETA_IMAGENES = "imagenes/"
 
-# Numero de empresas a generar
-NUMERO_DE_EMPRESAS = 1
 
 ##### GENERACION DE CORREOS #####
 GENERAR_CORREOS = False # En caso de querer generar los correos aleatoriamente
